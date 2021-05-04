2a140a2fba2652eee5cb11b301cea48789558823
AndrewHolovlov <holovlovandrii@gmail.com>
2021-05-05 00:50:02 +0300

    fix config

--- config.py
+++ config.py
@@ -3,14 +3,11 @@ from environs import Env
 env = Env()
 env.read_env()
 
-# BOT_TOKEN = env("BOT_TOKEN")
-BOT_TOKEN = '1519055784:AAH9iiP1flUP6zK3QW3rJXqwFQnGI_q1isQ'
-# HOST = env("HOST")
+BOT_TOKEN = env("BOT_TOKEN")
+HOST = env("HOST")
 
-# DB_HOST = env("DB_HOST")
-# DB_NAME = env("DB_NAME")
-DB_NAME = 'courses_bot_db'
-# DB_USER = env("DB_USER")
-DB_USER = 'admin'
-# DB_PASSWORD = env("DB_PASSWORD")
-DB_PASSWORD = '0627'
+DB_HOST = env("DB_HOST")
+DB_NAME = env("DB_NAME")
+DB_USER = env("DB_USER")
+DB_PASSWORD = env("DB_PASSWORD")
+
