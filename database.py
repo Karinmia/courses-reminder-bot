78ccce7bc1bcbba4cd5248f0b43bac263e361b59
Karinmia <karinmia99avr@gmail.com>
2021-06-13 16:18:06 +0300

    fix db connection

--- database.py
+++ database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from config import DB_NAME, DB_USER, DB_PASSWORD
+from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
 
 
-engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}')
+engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}')
 
 Session = sessionmaker(bind=engine)
 session = Session()
