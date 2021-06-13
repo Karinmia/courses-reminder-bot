d54eb8a37b98ff5cb7a2e2f1c9c6c8b242f44188
Karinmia <karinmia99avr@gmail.com>
2021-06-13 16:51:12 +0300

    fix server configuration

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
+import os
 from time import sleep
 
-import flask
+from flask import Flask, request
 from telebot import types, logger as telebot_logger
 
 from bot_handlers import bot
@@ -8,20 +9,36 @@ from config import BOT_TOKEN, HOST
 
 telebot_logger.setLevel(telebot_logger.DEBUG)
 
-server = flask.Flask(__name__)
+server = Flask(__name__)
 
-if bot.get_webhook_info().url != f"https://{HOST}/{BOT_TOKEN}":
-    bot.remove_webhook()
-    sleep(2)
-    bot.set_webhook(url=f"https://{HOST}/{BOT_TOKEN}", certificate=open('webhook_cert.pem', 'r'))
+# if bot.get_webhook_info().url != f"https://{HOST}/{BOT_TOKEN}":
+#     bot.remove_webhook()
+#     sleep(2)
+#     bot.set_webhook(url=f"https://{HOST}/{BOT_TOKEN}", certificate=open('webhook_cert.pem', 'r'))
 
 
 @server.route('/' + BOT_TOKEN, methods=['POST'])
 def get_message():
-    bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode('utf-8'))])
+    bot.process_new_updates([types.Update.de_json(request.stream.read().decode('utf-8'))])
     return 'OK', 200
 
 
-@server.route('/', methods=['GET'])
-def index():
-    return 'Home page', 200
+# @server.route('/', methods=['GET'])
+# def index():
+#     return 'Home page', 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=f'https://course-helper-bot-kpi.herokuapp.com/{BOT_TOKEN}')
+    return "!", 200
+
+
+if __name__ == "__main__":
+    if bot.get_webhook_info().url != f"https://{HOST}/{BOT_TOKEN}":
+        bot.remove_webhook()
+        sleep(2)
+        bot.set_webhook(url=f'https://course-helper-bot-kpi.herokuapp.com/{BOT_TOKEN}')
+
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
